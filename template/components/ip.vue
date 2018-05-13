f6158baabf0d3efb0385f46acd23334998e2aaff
lerte smith <lerte@qq.com>
2018-05-13 12:27:24 +0800

    Init Repo :tada:

---
+++ template/components/ip.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card>
+    Your public ip is: {{ip}}
+  </v-card>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Prop,
+  Vue
+} from "nuxt-property-decorator"
+@Component({})
+export default class Card extends Vue {
+  @Prop() ip
+}
+</script>
---
+++ template/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app light>
+    <v-toolbar class="white">
+      <v-toolbar-title></v-toolbar-title>
+    </v-toolbar>
+    <nuxt/>
+    <v-footer>
+      <v-layout row wrap align-center>
+        <v-flex xs12>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+  </v-app>
+</template>
